0cfbda27700e0ae9ba7fbbbcc00466d5db02da48
Jill Yeh <j456711282@gmail.com>
2022-06-20 17:10:26 +0800

    [WIP] Linked list practice

--- 3-LinkedLists/LinkedList-Skeleton.playground/Contents.swift
+++ 3-LinkedLists/LinkedList-Skeleton.playground/Contents.swift
@@ -12,28 +12,75 @@ class Node {
 
 class LinkList {
     private var head: Node?
-        
+   
+    // O(1)
     func addFront(_ data: Int) {
+        let node = Node(data, head)
+        head = node
     }
 
+    // O(1)
     func getFirst() -> Int? {
-        return 0
+        if head == nil {
+            return nil
+        }
+        return head!.data
     }
 
+    // O(n)
     func addBack(_ data: Int) {
+        let newNode = Node(data)
+        if head == nil {
+            head = newNode
+            return
+        }
+        
+        var node = head
+        while node?.next != nil {
+            node = node?.next
+        }
+        node?.next = newNode
     }
 
+    // O(n)
     func getLast() -> Int? {
-        return nil
+        if head == nil { return nil }
+        
+        var node = head
+        while node?.next != nil {
+            node = node?.next
+        }
+        return node?.data
     }
 
+    // O(n)
     func insert(position: Int, data: Int) {
+        if position == 0 {
+            addFront(data)
+            return
+        }
+
+        let newNode = Node(data)
+        var currentNode = head
+        for _ in 0 ..< (position - 1) {
+            currentNode = currentNode?.next
+        }
+        newNode.next = currentNode?.next
+        currentNode?.next = newNode
     }
     
     func deleteFirst() {
+        head = head?.next
     }
 
     func deleteLast() {
+        var previousNode: Node? = nil
+        var currentNode = head
+        while currentNode?.next != nil {
+            previousNode = currentNode
+            currentNode = currentNode?.next
+        }
+        previousNode?.next = nil
     }
     
     func delete(at position: Int) {
@@ -63,4 +110,22 @@ class LinkList {
 }
 
 let linkedList = LinkList()
+linkedList.addFront(3)
+linkedList.addFront(2)
+linkedList.addFront(1)
+linkedList.printLinkedList()
 
+linkedList.addFront(4)
+linkedList.printLinkedList()
+
+linkedList.addBack(5)
+linkedList.printLinkedList()
+
+linkedList.addBack(2)
+linkedList.getLast()
+
+linkedList.insert(position: 8, data: 8)
+linkedList.printLinkedList()
+
+linkedList.deleteFirst()
+linkedList.printLinkedList()
